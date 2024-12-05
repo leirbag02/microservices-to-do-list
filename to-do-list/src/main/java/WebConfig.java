1b7a6dbd72cea17bb11ee3c90080b9bfad9c67d8
Gabriel Lopes <gabrielseverino.lopesguimares@jala.university>
2024-12-05 00:11:20 -0300

    l

--- to-do-list/src/main/java/WebConfig.java
+++ to-do-list/src/main/java/WebConfig.java
@@ -3,7 +3,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class WebConfig {
 
@@ -11,10 +10,10 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings( CorsRegistry registry) {
+            public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://flutter-repository-a-itodolist-zvks.vercel.app")
-                        .allowedMethods("*")
+                        .allowedOrigins("https://flutter-repository-a-itodolist-qkvm-8hax-c6cs-9l9arc4ty.vercel.app") // Especifique o domínio do seu app Flutter
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
         };
